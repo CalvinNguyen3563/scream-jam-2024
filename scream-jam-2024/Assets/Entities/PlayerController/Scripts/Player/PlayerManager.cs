b52fd585c190c48086c89e3ab8ad823a71a1c962
Skyglader <andyhoangnguyen2005@gmail.com>
2024-10-14 22:27:57 -0700

    add enemy

---
+++ scream-jam-2024/Assets/Entities/PlayerController/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,54 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    [Header("Player Components")]
+    public Rigidbody rb;
+    public Transform orientation;
+    public Camera cam;
+    public CinemachineVirtualCamera vcam;
+    public GameObject cameraHolder;
+    public PlayerAnimatorManager playerAnimatorManager;
+    public PlayerInteractableManager playerInteractableManager;
+    public Collider playerCollider;
+
+    [Header("Player Classes")]
+    public PlayerLocomotionManager playerLocomotionManager;
+
+    [Header("Ground Check")]
+    public Transform legs;
+    public bool isGrounded;
+    public float groundCheckRadius = 2f;
+    public LayerMask whatIsGround;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerLocomotionManager = GetComponent<PlayerLocomotionManager>();
+    }
+    void Start()
+    {
+        playerAnimatorManager = WorldGameObjectStorage.Instance.cameraHolder.GetComponentInChildren<PlayerAnimatorManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DetectGround();
+    }
+
+    public void DetectGround()
+    {
+        isGrounded = Physics.OverlapSphere(legs.position, groundCheckRadius, whatIsGround).Length > 0;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere(legs.position, groundCheckRadius);
+    }
+}
---
+++ scream-jam-2024/Assets/Entities/PlayerController/Scripts/Player/WorldGameObjectStorage.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGameObjectStorage : MonoBehaviour
+{
+    public static WorldGameObjectStorage Instance;
+    public PlayerManager player;
+    public GameObject cameraHolder;
+    public GameObject mainCam;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+
+    }
+}
